5f05187cdccb7b8a4787b5b5cbcdfe34c2865c50
Adam Shepherd <ashepherd@whoi.edu>
2017-10-11 14:36:00 -0400

    uses Go to serve vocabulary

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
-FROM nginx
+FROM golang:1.9
 
-# nginx already exposes port 80
+WORKDIR /go/src/app
+ADD ./server.go .
+ADD ./html/ ./html
 
-# VOLUMES
-# - ./html:/usr/share/nginx/html
-# - ./nginx.conf:/etc/nginx/nginx.conf:ro
-# - ./logs:/var/log/nginx/
+RUN go-wrapper download   # "go get -d -v ./..."
+RUN go-wrapper install    # "go install -v ./..."
+
+CMD ["go-wrapper", "run"] # ["app"]
---
+++ server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+  "log"
+  "net/http"
+
+  "github.com/gorilla/mux"
+)
+
+// MyServer struct for mux router
+type MyServer struct {
+  r *mux.Router
+}
+
+func main() {
+
+  htmlRouter := mux.NewRouter().StrictSlash(true)
+  htmlRouter.PathPrefix("/static").Handler(http.StripPrefix("/static", http.FileServer(http.Dir("./html/static"))))
+  htmlRouter.HandleFunc("/voc/", func(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, "./html/voc/index.html")
+  })
+  htmlRouter.HandleFunc("/voc/{resource}", func(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, "./html/voc/index.html")
+  })
+  http.Handle("/", &MyServer{htmlRouter})
+
+  err := http.ListenAndServe(":9900", nil)
+  // http 2.0 http.ListenAndServeTLS(":443", "server.crt", "server.key", nil)
+  if err != nil {
+    log.Fatal(err)
+  }
+}
+
+func (s *MyServer) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+  rw.Header().Set("Access-Control-Allow-Origin", "*")
+  rw.Header().Set("Access-Control-Allow-Methods", "POST, GET")
+  rw.Header().Set("Access-Control-Allow-Headers",
+    "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+  // Let the Gorilla work
+  s.r.ServeHTTP(rw, req)
+}
+
+func addDefaultHeaders(fn http.HandlerFunc) http.HandlerFunc {
+  return func(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    fn(w, r)
+  }
+}
